15b847ca6db78ee82f172dc39cc5caf356add989
marc-etti <andrea04.marchetti@edu.unife.it>
2025-12-01 17:53:27 +0100

    correzioni

--- providers.tf
+++ providers.tf
@@ -2,18 +2,30 @@ terraform {
   required_version = ">= 1.10.0"
 
   required_providers {
+    azapi = {
+      source  = "azure/azapi"
+      version = "~> 1.5"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
     }
   }
 }
 
 provider "azurerm" {
-  features {}
+  features {
+    resource_group {
+      prevent_deletion_if_contains_resources = false
+    }
+  }
   client_id       = var.appId
   client_secret   = var.password
   tenant_id       = var.tenant
-  
+  display_name    = var.displayName
   subscription_id = var.subscription_id
 }
--- variables.tf
+++ variables.tf
@@ -1,23 +1,28 @@
 variable "location" {
   type    = string
   default = "switzerlandnorth"
+  description = "Azure region where resources will be created"
 }
 
 variable "vm_name" {
   type    = string
+  description = "Name of the virtual machine"
 }
 
 variable "admin_username" {
   type    = string
   default = "azureuser"
+  description = "Admin username for the virtual machine"
 }
 
 variable "ssh_public_key" {
   type = string
+  description = "SSH public key for the virtual machine"
 }
 
 variable "subscription_id" {
   type = string
+  description = "Azure subscription ID"
 }
 
 # Azure service principal credentials
